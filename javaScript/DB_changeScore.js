ef670b0306ecfabb13fa3bef11e989bc19fa2aae
ACupofAir <timethoughts@outlook.com>
2020-12-13 17:01:22 +0800

    Add a script for top.zhan

---
+++ javaScript/DB_changeScore.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  addButton("changeScore", changeScore);
+});
+
+function addButton(text, onclick, cssObj) {
+  cssObj = cssObj || {
+    position: "absolute",
+    bottom: "10%",
+    left: "45%",  
+    "z-index": 15,
+    color: "#F72670",
+    background: "#2ecc71",
+  };              
+  let button = document.createElement("button"),
+    btnStyle = button.style;
+  document.body.appendChild(button);
+  button.innerHTML = text;
+  button.onclick = onclick;
+  Object.keys(cssObj).forEach((key) => (btnStyle[key] = cssObj[key]));
+  return button;  
+}                                                                      
+                                                                       
+function changeScore() {                                               
+  var score = 27;                                                      
+  document.getElementsByClassName("sbq state1")[0].style = "left: 86.77003%";
+  document.getElementsByClassName("shk")[0].style = "width: 90%";      
+  document.getElementsByClassName(                                     
+    "js_listen_score font-big"                                         
+  )[0].innerHTML = score;                                              
+  document.querySelector(                                              
+    "body > div.wrap.zhan > div.main.zhan-main.main-single > div > div.floatlayer.single-listen > div > p > i.font-big.js_listen_score"
+  ).innerHTML = score;                                                 
+  alert("Change Successly!");                                          
+}                                                                      
+                                                                       
